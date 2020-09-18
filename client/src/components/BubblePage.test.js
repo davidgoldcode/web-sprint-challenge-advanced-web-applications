1e2cd3174f35f4dd140749e7940685976171e164
David Gold <davidgold@hey.com>
2020-09-18 12:29:21 -0400

    have test in place, but having issues with useParams on react testing library

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,11 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import BubblePage from "./BubblePage";
+import PrivateRoute from "./PrivateRoute";
 
-test("Fetches data and renders the bubbles", () => {
-  // Finish this test
+test("Fetches data and renders the bubbles", async () => {
+  render(<BubblePage/>)
+
+  await waitFor(() => screen.getAllByText(<li></li>))
+  expect(screen.getAllByText(<li></li>).toHaveLength(12))
 });
